418b8cb2bce404ff05ca9a78e906b4f4f81e8cfa
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 20:12:04 +0530

    Square or rectangle

---
+++ weekly_task/week_2/q.10 rectangle.py
@@ -0,0 +1,7 @@
+#Take values of length and breadth of a rectangle from user and check if it is square or not.
+length=int(input("Enter the length of a rectangle: "))
+breadth=int(input("Enter the breadth of a rectangle: "))
+if(length==breadth):
+    print("it is a square")
+else:
+    print("It is a rectangle")
