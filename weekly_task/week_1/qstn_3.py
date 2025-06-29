a667ef5d1a2f8dbb70d74e0309416e4475f5b168
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 14:00:23 +0530

    Arithmetic Operations on 2 numbers

---
+++ weekly_task/week_1/qstn_3.py
@@ -0,0 +1,10 @@
+#6. Write a Python program that takes two numbers as input from the user and performs arithmetic operations
+# (addition, subtraction, multiplication, division, and modulus) on them.
+# Display the results of each operation.
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+print("the sum is: ",num1+num2)
+print("the deducted value is: ",num1-num2)
+print("multiplied value is: ",num1*num2)
+print("the divided value is: ",num1/num2)
+print("the modulus value is: ",num1%num2)
