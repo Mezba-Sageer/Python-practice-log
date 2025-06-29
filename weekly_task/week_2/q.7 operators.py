83c520f75fa0b33d2f9cb3b235022725bde07f8e
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 20:08:45 +0530

    Types of mathematical operations performed

---
+++ weekly_task/week_2/q.7 operators.py
@@ -0,0 +1,20 @@
+#Q7. Write a program to accept two numbers and mathematical operators and perform operation accordingly.
+
+
+num1=int(input("enter no.1: "))
+num2=int(input("enter no.2: "))
+operator=input("Enter an operator (+,-,/,*): ")
+if(operator=='+'):
+    sum=num1+num2
+    print("the sum is: ",sum)
+elif(operator=='-'):
+    difference=num1-num2
+    print("the difference is: ",difference)
+elif(operator=='*'):
+    product=num1*num2
+    print("the product is: ",product)
+elif(operator=='/'):
+    division=num1/num2
+    print("The division value is: ",division)
+else:
+    print("invalid operator")
