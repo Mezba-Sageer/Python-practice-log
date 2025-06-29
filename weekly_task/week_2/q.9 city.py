4e9304a77b046e5129a592592949dc676e87b7bd
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 20:09:56 +0530

    Monument of corresponding city

---
+++ weekly_task/week_2/q.9 city.py
@@ -0,0 +1,11 @@
+#Accept any city from the user and display monument of that city.
+city=input("enter a city: ").lower()
+if(city=="agra"):
+    print("city:",city,"Monument: Taj Mahal")
+elif(city == "delhi"):
+    print("city:",city,"Monument: Red Fort")
+elif(city == "jaipur"):
+    print("city:",city,"Monument: Jal Mahal")
+else:
+    print("enter another city")
+
