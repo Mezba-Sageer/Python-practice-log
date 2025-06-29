6ddd4e28bc843b3130325092f826278de706cc6f
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 13:29:02 +0530

    added script to sum three numbers

---
+++ weekly_task/week_1/qstn_1.py
@@ -0,0 +1,7 @@
+#question 3 :- Write a Python program that takes three numbers
+# as input from the user and prints out the sum of those numbers.
+
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+num3=int(input("Enter number 3: "))
+print("The sum of the three numbers is:",num1+num2+num3)
