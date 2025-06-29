88efff480a49539342540537c398bc5d6beff539
Mezba Sageer <mezbasageer19@gmail.com>
2025-06-29 14:00:55 +0530

    Comparison Operations on 2 numbers

---
+++ weekly_task/week_1/qstn_4.py
@@ -0,0 +1,11 @@
+#Create a Python program that prompts the user to enter two numbers. Compare the numbers using
+# comparison operators (>, <, ==,!=, >=, <=) and print out the result of each comparison.
+num1=int(input("Enter no.1: "))
+num2=int(input("Enter no.2: "))
+print("num1>num2: ",num1>num2)
+print("num1<num2: ",num1<num2)
+print("num1==num2",num1==num2)
+print("num1!=num2: ",num1!=num2)
+print("num1>=num2",num1>=num2)
+print("num1<=num2",num1<=num2)
+
